ede5fb41e6057acbc9add4c89f4b71928b1a0c0a
Mateusz Fislak <mfislak@valuelogic.one>
2021-10-31 16:29:43 +0100

    add session termination after timeout

--- src/main/java/com/interview/collibra/MessageServer.java
+++ src/main/java/com/interview/collibra/MessageServer.java
@@ -1,6 +1,8 @@
 package com.interview.collibra;
 
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -9,41 +11,83 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class MessageServer {
 
+    public static final String FAREWELL_PATTERN = "BYE %s, WE SPOKE FOR %s MS";
+    public static final int SESSION_TIMEOUT = 30;
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private Instant timeOfLastMessage;
+    private Instant timeOfSessionStart;
+    private String clientName;
 
     public void init() throws IOException {
         serverSocket = new ServerSocket(5000);
         clientSocket = serverSocket.accept();
+        timeOfSessionStart = Instant.now();
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        sendMessage(String.format("HI, I AM %s", UUID.randomUUID()));
     }
+
+    public void sendMessage(String message) {
+        System.out.printf("[Server] -> %s%n", message);
+        out.println(message);
+    }
+
 
     @Async
     public void listenToMessages() throws IOException {
         String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println(String.format("[Server] <- %s", inputLine));
-            if (".".equals(inputLine)) {
-                out.println("good bye");
-                break;
+        while (in != null & (inputLine = in.readLine()) != null) {
+            timeOfLastMessage = Instant.now();
+            System.out.printf("[Server] <- %s%n", inputLine);
+            if (inputLine.startsWith("HI, I AM ")) {
+                clientName = inputLine.split("HI, I AM ")[1];
+                sendMessage(String.format("HI %s", clientName));
             }
-            String response = String.format("Hello %s", inputLine);
-            System.out.println(String.format("[Server] -> %s", response));
-            out.println(response);
         }
     }
 
+    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.SECONDS)
+    public void listenForSessionTimeout() throws IOException {
+        if (clientSocket != null && !clientSocket.isClosed()) {
+            final Duration durationFromLastMessage = Duration.between(timeOfLastMessage, Instant.now());
+            if ((durationFromLastMessage.getSeconds() > SESSION_TIMEOUT)) {
+                terminateClientSession();
+            }
+        }
+    }
+
+    private void terminateClientSession() throws IOException {
+        final Duration sessionDuration = Duration.between(timeOfSessionStart, Instant.now());
+        String farewellMessage = String.format(FAREWELL_PATTERN, clientName, sessionDuration.toMillis());
+        sendMessage(farewellMessage);
+        closeClient();
+
+    }
+
+
     public void stop() throws IOException {
-        in.close();
-        out.close();
+        closeClient();
+        closeServer();
+    }
+
+    private void closeClient() throws IOException {
         clientSocket.close();
+        out.close();
+    }
+
+    private void closeServer() throws IOException {
+        in.close();
         serverSocket.close();
     }
 }
